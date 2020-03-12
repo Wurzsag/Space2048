cf3facfca850457a297357c2f6b2f5c78573e527
Wurzsag <ekku.poh@gmail.com>
2020-03-12 04:46:01 +0200

    Implement main menu

--- app/src/main/java/fi/tuni/space2048/MainActivity.java
+++ app/src/main/java/fi/tuni/space2048/MainActivity.java
@@ -2,7 +2,9 @@ package fi.tuni.space2048;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View button) {
+        //Intent i = new Intent(this, GameActivity.class);
+        //startActivity(i);
+    }
+
+    public void showScore(View button) {
+        //Intent i = new Intent(this, HighScoreActivity.class);
+        //startActivity(i);
     }
 }
